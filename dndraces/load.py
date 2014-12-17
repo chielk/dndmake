3dc603a46a723f47db2cc75d3304367cb7b9764e
Chiel Kooijman <chiel999@gmail.com>
2014-12-17 21:26:46 +0100

    Race loader can now load class-names with hyphen.

--- dndraces/load.py
+++ dndraces/load.py
@@ -1,35 +1,41 @@
 from importlib.machinery import SourceFileLoader
+import re
 import sys
 import os
 
 
 def load_race(name):
-    name = name.lower()
+    base_name = name.lower()
+    file_name = base_name.replace('-', '_') + ".py"
+    class_name = ''.join(part.capitalize() for part in base_name.split('-'))
+
     HOME = os.getenv("HOME")
     RACES = os.path.join(HOME, ".dndmake", "races")
     os.makedirs(RACES, exist_ok=True)
 
     try:
         # from this library
-        Race = getattr(__import__("dndraces"), name.capitalize())
+        Race = getattr(__import__("dndraces"), class_name)
     except AttributeError:
         # Don't make __pycache__ file in the races directories
         sys.dont_write_bytecode = True
         try:
             # from $HOME
-            loader = SourceFileLoader(name.capitalize(),
-                                      os.path.join(RACES, name + ".py"))
+            loader = SourceFileLoader(class_name,
+                                      os.path.join(RACES, file_name))
             races = loader.load_module()
-            Race = getattr(races, name.capitalize())
+            Race = getattr(races, class_name)
         except FileNotFoundError:
             try:
                 # from /etc
                 RACES = "/etc/dndmake/races"
-                loader = SourceFileLoader(name.capitalize(),
-                                          os.path.join(RACES, name + ".py"))
+                loader = SourceFileLoader(class_name,
+                                          os.path.join(RACES, file_name))
                 races = loader.load_module()
-                Race = getattr(races, name.capitalize())
+                Race = getattr(races, class_name)
             except FileNotFoundError:
-                raise FileNotFoundError("Can not find class for " + name)
+                msg = "Can not find class for " + name + "\n"
+                msg += 'Looking for class "' + class_name + '" in ' + file_name
+                raise FileNotFoundError()
 
     return Race
