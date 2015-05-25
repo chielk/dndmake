36cff544116e889162aa8af8f403efabfa256a20
Chiel Kooijman <chiel999@gmail.com>
2015-05-25 16:01:43 +0200

    Fix bug when ~/.dndmake doesn't exist yet.

--- dndraces/load.py
+++ dndraces/load.py
@@ -8,20 +8,24 @@ HOME_RACES = os.path.join(HOME, ".dndmake", "races")
 ETC_RACES = "/etc/dndmake/races"
 
 
+def ensure_directories():
+    HOME = os.getenv("HOME")
+    HOME_RACES = os.path.join(HOME, ".dndmake", "races")
+    os.makedirs(HOME_RACES, exist_ok=True)
+
+
 def list_races():
+    ensure_directories()
     classes = os.listdir(HOME_RACES)
     classes += os.listdir(ETC_RACES)
     return [cls.split('.')[0].replace('_', '-') for cls in classes]
 
 
 def load_race(name):
+    ensure_directories()
     base_name = name.lower()
     file_name = base_name.replace('-', '_') + ".py"
     class_name = ''.join(part.capitalize() for part in base_name.split('-'))
-
-    HOME = os.getenv("HOME")
-    HOME_RACES = os.path.join(HOME, ".dndmake", "races")
-    os.makedirs(HOME_RACES, exist_ok=True)
 
     try:
         # from this library
